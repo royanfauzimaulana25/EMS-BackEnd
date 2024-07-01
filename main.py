eff20f78c0aaf6c6dbe56500a8928bc9c66d0e35
Muh Royan Fauzi Maulana <royanfauzimaulana25@gmail.com>
2024-07-01 20:24:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -220,4 +220,4 @@ async def get_lomba(id) :
     return response
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    uvicorn.run("main:app", host="0.0.0.0", port=int(8000))
